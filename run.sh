92ffe835a13d14c510fce8328d2d31efff2bb9b9
wuyonghuang <15767059448@qq.com>
2025-03-19 00:12:53 +0800

    update

--- run.sh
+++ run.sh
@@ -82,7 +82,7 @@ current_time=$(date +"%Y-%m-%d-%H:%M:%S")
 # 	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
 # 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
 
-# note: DPO 是可以运行的
+# DPO 是可以运行的
 # CUDA_VISIBLE_DEVICES=$1  python train.py \
 #     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
 # 	--split='train' \
@@ -100,17 +100,113 @@ current_time=$(date +"%Y-%m-%d-%H:%M:%S")
 
 
 # fail: 将GRPO的奖励修改成离散
+# CUDA_VISIBLE_DEVICES=$1  python train.py \
+#     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
+# 	--split='train' \
+# 	--batch_size=8 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=0.05 \
+# 	--img_size=512 \
+# 	--warmup --AdamW --max_epochs=300 --stop_epoch=300 \
+# 	--vit_name='vit_l' \
+# 	--n_gpu 1 \
+# 	--model 'sam2' \
+# 	--is_grpo --rw_dispered \
+# 	--interval_epoch=1 \
+# 	--num_prompts_per_class=3 \
+# 	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
+# 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
+
+
+# 增加 采样数量为 8
+# CUDA_VISIBLE_DEVICES=$1  python train.py \
+#     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
+# 	--split='train' \
+# 	--batch_size=4 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=0.05 \
+# 	--img_size=224 \
+# 	--warmup --AdamW --max_epochs=3 --stop_epoch=3 \
+# 	--vit_name='vit_b' \
+# 	--n_gpu=1 \
+# 	--model='sam2' \
+# 	--is_grpo --rw_dispered \
+# 	--rw_temp=3 \
+# 	--grpo_KL_weight \
+# 	--weight_temp=0.5 \
+# 	--interval_epoch=1 \
+# 	--num_prompts_per_class=8 \
+# 	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
+# 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
+
+# 增加采样数量为16
+# CUDA_VISIBLE_DEVICES=$1  python train.py \
+#     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
+# 	--split='train' \
+# 	--batch_size=2 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=0.05 \
+# 	--img_size=224 \
+# 	--warmup --AdamW --max_epochs=3 --stop_epoch=3 \
+# 	--vit_name='vit_b' \
+# 	--n_gpu=1 \
+# 	--model='sam2' \
+# 	--is_grpo --rw_dispered \
+# 	--rw_temp=3 \
+# 	--grpo_KL_weight \
+# 	--weight_temp=0.5 \
+# 	--interval_epoch=1 \
+# 	--num_prompts_per_class=16 \
+# 	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
+# 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
+
+
+# 增加采样数量为8, 使用 f1 奖励函数, KL 权重设置为 1
+# CUDA_VISIBLE_DEVICES=$1  python train.py \
+#     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
+# 	--split='train' \
+# 	--batch_size=4 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=1 \
+# 	--img_size=224 \
+# 	--warmup --AdamW --max_epochs=3 --stop_epoch=3 \
+# 	--vit_name='vit_b' \
+# 	--n_gpu=1 \
+# 	--model='sam2' \
+# 	--is_grpo --rw_dispered --rw_func='f1' \
+# 	--rw_temp=3 \
+# 	--grpo_KL_weight \
+# 	--weight_temp=0.5 \
+# 	--interval_epoch=1 \
+# 	--num_prompts_per_class=8 \
+# 	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
+# 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
+
+# # dev
+# CUDA_VISIBLE_DEVICES=$1  python train.py \
+#     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
+# 	--split='train' \
+# 	--batch_size=4 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=1 \
+# 	--img_size=224 \
+# 	--warmup --AdamW --max_epochs=3 --stop_epoch=3 \
+# 	--vit_name='vit_b' \
+# 	--n_gpu=1 \
+# 	--model='sam2' \
+# 	--dev \
+# 	--interval_epoch=1 \
+# 	--num_prompts_per_class=8 \
+# 	> /database/wuyonghuang/hsam_code/output/dev-${current_time}.log 2>&1 \
+# 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
+
+
+# DPO
 CUDA_VISIBLE_DEVICES=$1  python train.py \
     --root_path="/database/wuyonghuang/hsam_code/data/multi-organ-CT/train_npz_512" \
 	--split='train' \
 	--batch_size=8 --base_lr=0.0001 --weight_decay=0.01 --kl_beta=0.05 \
-	--img_size=512 \
-	--warmup --AdamW --max_epochs=300 --stop_epoch=300 \
-	--vit_name='vit_l' \
+	--img_size=224 \
+	--warmup --AdamW --max_epochs=50 \
+	--vit_name='vit_b' \
 	--n_gpu 1 \
 	--model 'sam2' \
-	--is_grpo --rw_dispered \
+	--is_dpo \
+	--prompt_type='point' \
+	--pos_point_num="(1, 5)" \
+	--neg_point_num="(0, 3)" \
+	--kl_prompt_type='both' \
 	--interval_epoch=1 \
 	--num_prompts_per_class=3 \
-	> /database/wuyonghuang/hsam_code/output/${current_time}.log 2>&1 \
+	> /database/wuyonghuang/hsam_code/output/DPO-${current_time}.log 2>&1 \
 	|| python -c "from sende import let_me_know; let_me_know('Error', 'SAM-Exps')"
--- run_test.sh
+++ run_test.sh
@@ -8,21 +8,45 @@ folders=(
 )
 
 
-# 遍历文件夹数组
-for folder in "${folders[@]}"; do
-    # 使用 find 命令查找所有 .pth 文件
-    pth_files=$(find "$folder" -type f -name "*.pth" | sort)
+# # 遍历文件夹数组
+# for folder in "${folders[@]}"; do
+#     # 使用 find 命令查找所有 .pth 文件
+#     pth_files=$(find "$folder" -type f -name "*.pth" | sort)
     
-    # 将找到的 .pth 文件传递给 Python 脚本
-    while IFS= read -r pth_file; do
-		current_time=$(date +"%Y-%m-%d-%H:%M:%S")
+#     # 将找到的 .pth 文件传递给 Python 脚本
+#     while IFS= read -r pth_file; do
+# 		current_time=$(date +"%Y-%m-%d-%H:%M:%S")
 
-		CUDA_VISIBLE_DEVICES=$1 python test.py \
-		--stage=3 --img_size=512 --model=sam2 --vit_name="vit_l" \
-		--is_savenii \
-		--ckpt=$pth_file \
-		--prompt_type='point'  \
-		> /database/wuyonghuang/hsam_code/test_outputs/${current_time}.log
+# 		CUDA_VISIBLE_DEVICES=$1 python test.py \
+# 		--stage=3 --img_size=512 --model=sam2 --vit_name="vit_l" \
+# 		--is_savenii \
+# 		--ckpt=$pth_file \
+# 		--prompt_type='point'  \
+# 		> /database/wuyonghuang/hsam_code/test_outputs/${current_time}.log
 
-    done <<< "$pth_files"
-done
+#     done <<< "$pth_files"
+# done
+
+
+current_time=$(date +"%Y-%m-%d-%H:%M:%S")
+
+# CUDA_VISIBLE_DEVICES=$1 python test.py \
+#   --stage=3 --img_size=512 --model=sam2 --vit_name="vit_l" \
+#   --is_savenii \
+#   --prompt_type='point'  \
+#   --point_strict \
+#   > /database/wuyonghuang/hsam_code/test_outputs/${current_time}.log
+
+
+# CUDA_VISIBLE_DEVICES=$1 python test.py \
+#   --stage=3 --img_size=512 --model=sam2 --vit_name="vit_l" \
+#   --is_savenii \
+#   --prompt_type='box'  \
+#   > /database/wuyonghuang/hsam_code/test_outputs/${current_time}.log
+
+CUDA_VISIBLE_DEVICES=$1 python test.py \
+  --stage=3 --img_size=224 --model=sam2 --vit_name="vit_b" \
+  --is_savenii \
+  --prompt_type='point'  \
+  --ckpt="/database/wuyonghuang/hsam_code/output/EXPS1/exp1/sam2-dpo/20250317_175612_Synapse_224_pretrain_vit_b_epo20_bs8_lr0.0001_s2345/epoch_000_loss-0.0359201457661887_iou-0.6986421409994364.pth" \
+  > /database/wuyonghuang/hsam_code/test_outputs/DPO-${current_time}.log
